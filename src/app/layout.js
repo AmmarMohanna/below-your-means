7a1a4bfcea386744f45e990fe950979fce57b9a9
Ammar Mohanna <Ammar@MacBook-Air-IT-3.local>
2026-06-14 00:14:19 +0300

    feat: use custom favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,9 +6,13 @@ export const metadata = {
   manifest: "/manifest.json",
   icons: {
     icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/icons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/icons/icon-192x192.png", sizes: "192x192", type: "image/png" },
       { url: "/icons/icon-512x512.png", sizes: "512x512", type: "image/png" },
     ],
+    shortcut: ["/favicon.ico"],
     apple: [{ url: "/icons/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
   },
   appleWebApp: {
